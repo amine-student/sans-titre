35ccc54383c00c9b552bd69c9b7d1c5b319ab59a
amine-student <65138500+amine-student@users.noreply.github.com>
2022-07-11 05:47:42 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    if (Mypass == "123C") {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            # # . . .
+            # . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        Mypass = ""
+    }
+})
+let MyInput = ""
+let Mypass = ""
+keypad.setKeyPad4(
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13,
+DigitalPin.P12,
+DigitalPin.P8,
+DigitalPin.P2,
+DigitalPin.P1,
+DigitalPin.P0
+)
+basic.forever(function () {
+    MyInput = keypad.getKeyString()
+    if (MyInput != "") {
+        Mypass += MyInput
+        basic.showString(MyInput)
+        basic.clearScreen()
+    }
+})
